bc8a077ac766d66c863dc1c892934f74eee0a540
Bose-creator <irawomartins@gmail.com>
2023-03-17 09:43:52 -0700

    Last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,14 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
+	else if (n < )
 	{
 		printf("%d is negative\n", n);
 	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
-}i
+
+}
