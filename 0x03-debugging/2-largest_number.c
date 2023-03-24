d7878cc22b77362f3971b518e92ef2d152d5f9f6
Bose-creator <irawomartins@gmail.com>
2023-03-23 17:22:44 -0700

    larget num

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,22 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 	largest = a;
 	}
-
-	else if (b > a && b > a)
+	else if (b > c)
 	{
 	largest = b;
-	}
-	else if (c > b)
-	{
-	largest = c;
 	}
 	else
 	{
-	largest = b;
+	largest = c;
 	}
+
 	return (largest);
 }
